a6297c085944200c7bbc28a52d850dad0c3f839b
Laxmi prasanna <laxmiprasanna59@gmail.com>
2021-09-12 12:56:10 +0530

    initial functionality

---
+++ script.js
@@ -0,0 +1,58 @@
+// dom elements
+
+const btnAddTask = document.querySelector(".task-input-btn");
+const btnClearAll = document.querySelector(".clear-all-btn");
+
+const taskInput = document.querySelector(".task-input");
+const taskContainer = document.querySelector(".list");
+const taskCountEl = document.querySelector(".task-count");
+
+//initial values
+
+let taskCount = 0;
+
+
+//event listeners
+btnAddTask.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", addTaskOnEnter);
+
+document.addEventListener("click", deleteTask);
+btnClearAll.addEventListener("click", clearAllTasks);
+
+
+//functions
+function addTaskOnEnter(e) {
+  if (e.key === "Enter") addTask();
+}
+
+function addTask() {
+
+  if (!taskInput.value.trim()) return;
+
+  const taskHtml = `<li class="item">
+                    <p>${taskInput.value}</p>
+                    <div class="btn delete-btn"><i class="fas fa-trash"></i></div>
+                   </li>`;
+
+  taskContainer.insertAdjacentHTML("beforeend", taskHtml);
+  taskInput.value = "";
+  taskInput.focus();
+  taskCount++;
+  taskCountEl.textContent = taskCount;
+}
+
+function deleteTask(e) {
+
+  if (e.target.classList.contains("delete-btn")) {
+    e.target.closest(".item").remove();
+    taskCount--;
+    taskCountEl.textContent = taskCount;
+
+  }
+}
+
+function clearAllTasks() {
+  taskContainer.innerHTML = "";
+  taskCount = 0;
+  taskCountEl.textContent = taskCount;
+}
